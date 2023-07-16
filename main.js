9dac80bfca62f7b47e93714bed3ce39fb455dfb2
Thais C C e Silva <thaisccs@gmail.com>
2023-07-16 16:40:17 -0300

    Finalização de formulario

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 const form = document.getElementById("form-test");
-
+const campo1 = document.getElementById("number1");
+const campo2 = document.getElementById("number2");
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    const campo1 = document.getElementById("number1");
-    const campo2 = document.getElementById("number2");
+    const mensagem01 = "O número do campo 01 é maior do que o número do campo 02";
+    const mensagem02 = "O número do campo 01 é menor do que o número do campo 02";
+    const mensagem03 = "O número do campo 01 é igual ao número do campo 02";
+    const mensagemErro = "A comparação não foi possível. Entre os dados novamente."
 
-    if (campo1 > campo2){
-        alert("O número do campo 01 é maior do que o número do campo 02");
-    } else if (campo1 < campo2){
-        alert("O número do campo 01 é menor do que o número do campo 02");
-    } else {
-        alert("O número do campo 01 é igual ao número do campo 02");
-    }
+    if (campo1.value > campo2.value){
+        const containerMensagem01 = document.querySelector(".mensagem01")
+        containerMensagem01.innerHTML = mensagem01;
+        containerMensagem01.style.display = "block";
+    } else if (campo1.value < campo2.value){
+        const containerMensagem01 = document.querySelector(".mensagem01")
+        containerMensagem01.innerHTML = mensagem02;
+        containerMensagem01.style.display = "block";
+    } else if (campo1.value == campo2.value) {
+        const containerMensagem01 = document.querySelector(".mensagem01")
+        containerMensagem01.innerHTML = mensagem03;
+        containerMensagem01.style.display = "block";;
+    } 
 });
